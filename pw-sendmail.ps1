636dd18ab0ed3659ef72327a0eccd371a6207df5
Ezzio Moreira <enzziom@gmail.com>
2020-01-29 17:01:12 -0300

    Update pw-sendmail.ps1

--- pw-sendmail.ps1
+++ pw-sendmail.ps1
@@ -1,21 +1,30 @@
-$emailSmtpServer = "mail.server.com"
-$emailSmtpServerPort = "587"
-$emailSmtpUser = "email@domain.com"
-$emailSmtpPass = "password"
-$attachment = "dirsouce\arq.txt"
- 
-$emailMessage = New-Object System.Net.Mail.MailMessage
-$emailMessage.From = "email@domain.com"
-$emailMessage.To.Add( "email2@domain.com" )
-$emailMessage.Subject = "Testing e-mail powershell"
-$emailMessage.Attachments.Add( $attachment )
-$emailMessage.IsBodyHtml = $true
-$emailMessage.Body = @"
-<p>Here is a message <strong>HTML formatted</strong>.</p>
+#=========================send e-mail==============================#
+$SMTPServer = "mail.locfama.com.br"
+$SMTPPort = "587"
+$Username = "ti@locfama.com.br" 
+$Password = "Locf@m@2022"
+#==================================================================#
+
+#=======================destination================================#
+$to = "enzziom@gmail.com" 
+$attachment = "D:\destino\log\$data.txt"
+$subject = "Bakcup Cbirdex" 
+$body = @"
+<p>Here is a message by Ezzio Moreira <strong>HTML formatted</strong>.</p>
 <p>From your friendly neighborhood IT guy</p>
 "@
+
+#=================================================================#
+#$attachment = "D:\destino\log\domingo14072019.txt"
+$message = New-Object System.Net.Mail.MailMessage 
+$message.subject = $subject 
+$message.from = $username 
+$message.to.add($to)
+$message.IsBodyHtml = $true
+$message.body = $body
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+#$smtp.EnableSSL = $true
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+$smtp.send($message)
  
-$SMTPClient = New-Object System.Net.Mail.SmtpClient( $emailSmtpServer , $emailSmtpServerPort )
-#$SMTPClient.EnableSsl = $true
-$SMTPClient.Credentials = New-Object System.Net.NetworkCredential( $emailSmtpUser , $emailSmtpPass );
-$SMTPClient.Send( $emailMessage ) 
+ 
