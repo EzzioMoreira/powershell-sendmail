3e8f6c698565338d999ebe00357786b212958eaf
Ezzio Moreira <enzziom@gmail.com>
2020-01-29 16:26:47 -0300

    Update pw-sendmail.ps1

--- pw-sendmail.ps1
+++ pw-sendmail.ps1
@@ -1,23 +1,21 @@
-#=========================source=================================#
-$SMTPServer = "smtp.domain.com"
-$SMTPPort = "587"
-$Username = "mail@domain.com" 
-$Password = "password"
-#================================================================#
-
-#=======================destination==============================#
-$to = "destination@domain.com" 
-$subject = "Mensseger test powershell" 
-$body = "this email sent by powershell.exe.
-Signature,
-Beltrano Moreira"
-#=================================================================#
-
-$message = New-Object System.Net.Mail.MailMessage 
-$message.subject = $subject 
-$message.from = $username 
-$message.to.add($to)
-$message.body = $body
-$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
-$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
-$smtp.send($message)
+$emailSmtpServer = "mail.server.com"
+$emailSmtpServerPort = "587"
+$emailSmtpUser = "email@domain.com"
+$emailSmtpPass = "password"
+$attachment = "dirsouce\arq.txt"
+ 
+$emailMessage = New-Object System.Net.Mail.MailMessage
+$emailMessage.From = "email@domain.com"
+$emailMessage.To.Add( "email2@domain.com" )
+$emailMessage.Subject = "Testing e-mail powershell"
+$emailMessage.Attachments.Add( $attachment )
+$emailMessage.IsBodyHtml = $true
+$emailMessage.Body = @"
+<p>Here is a message <strong>HTML formatted</strong>.</p>
+<p>From your friendly neighborhood IT guy</p>
+"@
+ 
+$SMTPClient = New-Object System.Net.Mail.SmtpClient( $emailSmtpServer , $emailSmtpServerPort )
+#$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential( $emailSmtpUser , $emailSmtpPass );
+$SMTPClient.Send( $emailMessage ) 
